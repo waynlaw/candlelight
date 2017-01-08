a0a489bbb794658595c1b9706529073552de4728
kLawrence <ultimate1352@gmail.com>
2017-01-08 17:14:30 +0900

    update(view): add title

--- candlelight/screen/main/controller/ViewController.swift
+++ candlelight/screen/main/controller/ViewController.swift
@@ -10,12 +10,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.navigationItem.title = "자주 가는 게시판"
     }
 
     override func loadView() {
