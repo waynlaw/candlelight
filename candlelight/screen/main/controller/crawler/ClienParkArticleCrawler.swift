9ddd7b00cabccd251eb1074d7156c2b83a5cf6f7
Wayne <waynejo1024@gmail.com>
2017-05-09 21:57:44 +0900

    update(): Add Clien comment parsing function

--- candlelight/screen/main/controller/crawler/ClienParkArticleCrawler.swift
+++ candlelight/screen/main/controller/crawler/ClienParkArticleCrawler.swift
@@ -14,16 +14,17 @@ class ClienParkArticleCrawler: ArticleCrawler {
     }
 
     func getContent() -> Future<Article?, NoError> {
-        return AlamofireRequest(url).map(parseHTML)
+        return getComment().zip(AlamofireRequest(url)).map(parseHTML)
     }
 
-    func parseHTML(html: String) -> Article? {
+    func parseHTML(comments:[Comment], html: String) -> Article? {
         if let doc = HTML(html: html, encoding: .utf8) {
             let titleOption = doc.xpath("//div[contains(@class, 'title-subject')]").first?.text?.trim()
             let authorOption = doc.xpath("//span[contains(@class, 'contact-name')]/button").first?.text?.trim()
             let readCountOption = doc.xpath("//span[contains(@class, 'view-count')]").first?.text?.trim()
             let contentOption = doc.xpath("//div[contains(@class, 'post-content')]").first?.innerHTML
             let postTimeOption = doc.xpath("//div[contains(@class, 'post-time')]").first?.text?.trim()
+            let commentsOption = doc.xpath("//div[contains(@class, 'comment-row')]")
 
             guard let title = titleOption,
                   let readCountText = readCountOption,
@@ -31,16 +32,57 @@ class ClienParkArticleCrawler: ArticleCrawler {
                   let postTime = postTimeOption
                     else {
                 print("data is invalid")
-                return Article()
+                return nil
             }
 
             let author = authorOption ?? "" // TODO: should fix it when name is image.
-            let comments = [Comment]()
-
             let dd = Util.dateFromString(dateStr: postTime, format: "yyyy-MM-dd HH:mm:ss")
             let readCount = Int(readCountText.digitsOnly()) ?? 0
             return Article(title: title, author: author, readCount: readCount, content: content, regDate: dd, comments: comments)
         }
         return nil
+    }
+
+    func getComment() -> Future<[Comment], NoError> {
+        let commentUrl = buildCommentUrl()
+        return AlamofireRequestAsJson(commentUrl).map(parseComment)
+    }
+
+    func buildCommentUrl() -> String {
+        let documentId = self.url.digitsOnly()
+        return "https://www.clien.net/service/api/board/park/\(documentId)/comment?param=%7B%22order%22%3A%22date%22%2C%22po%22%3A0%2C%22ps%22%3A999999%7D"
+    }
+
+    func parseComment(_ response: DataResponse<Any>) -> [Comment] {
+        switch response.result {
+        case .success(let JSON):
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+
+            let response = JSON as! NSArray
+            var comments = [Comment]()
+            var reCommentSns = [Int:Bool?]()
+            for comment in response {
+                let commentDic = comment as! NSDictionary
+
+                let content = commentDic["comment"] as! String
+                let author = (commentDic["member"] as! NSDictionary!)["nick"] as! String
+                let regDate = commentDic["insertDate"] as! String
+                let reCommentSn = commentDic["reCommentSn"] as! Int
+
+                let dd = (formatter.date(from: regDate))
+
+                if reCommentSns[reCommentSn] != nil {
+                    comments.append(Comment(author: author, content: content, regDate: dd!, depth: 1))
+                } else {
+                    comments.append(Comment(author: author, content: content, regDate: dd!, depth: 0))
+                }
+                reCommentSns[reCommentSn] = true
+            }
+
+            return comments
+        case .failure(_):
+            return [Comment]()
+        }
     }
 }
